9a54f62a06c99cdb7c7ba7501bf9bddf4213ac8e
AndreyPavlov93 <grelllok@mail.ru>
2024-11-05 01:02:22 +0300

    Add files via upload

---
+++ module_5_1.py
@@ -0,0 +1,24 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print('Такого этажа не существует')
+        else:
+            for etazhi in range(1,new_floor+1):
+                print(etazhi)
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(19)
+h2.go_to(2)
+
+
+
+
+
+
+
